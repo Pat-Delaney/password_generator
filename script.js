f78a74abcbd02233441e1a060d922fba380db018
Patrick <patrick@Patricks-iMac.local>
2022-11-25 16:36:54 -0800

    implemented a new process using object properties instead of individual variables

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// var wantNumbers = null;
-// var wantLower = null;
-// var wantCap = null;
-// var wantSymbols = null;
-// var lower = "abcdefghijklmnopqrstuvwxyz";
-// var cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// var symbols = ` !"#$%&'()*+,-./:;<=>?@[\]^_{|}~`;
-// var numbers = "1234567890";
 
+var library = {
+  1 : "1234567890",
+  2 :" \"!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+  3 : "abcdefghijklmnopqrstuvwxyz",
+  4 : "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -17,81 +15,49 @@ function writePassword() {
   passwordText.value = password;
   
 }
-
-// function generatePassword(){
-  // var desiredLength = prompt("How long would you like your password?");
-  // desiredLength = parseInt(desiredLength);
-  // var pool = null;
-  // if (desiredLength < 8 || desiredLength > 128){
-  //  alert("Please choose a number between 8 and 128.");
-  
-  // }
-  // else{
-    // wantSymbols = confirm("Would you like symbols in your password?");
-    // wantCap = confirm("Would you like to have capital letters in your password?");
-    // wantLower = confirm("Would you like lowercase letters in your password?");
-    // wantNumbers = confirm("Would you like numbers in your password?");
-  //   if (wantSymbols){
-  //   pool += symbols;
-  //   }
-  //   if (wantCap){
-  //   pool += cap;
-  //   }
-  //   if (wantLower){
-  //     pool += lower;
-  //   }
-  //   if (wantNumbers){
-  //    pool += numbers;
-  //   }
-  //   if (!wantNumbers&&!wantCap&&!wantSymbols&&!wantLower){
-  //     alert("Unable to generate password based on criteria!")
-  //   }
-  // }
-  //   var result = "‎" ;
-  // for (var i = 0; i < desiredLength; i++){
-  //     result += pool[Math.floor(Math.random() * pool.length)];
-  //  }
-  
-  // console.log(pool);
-  // console.log(pool.length)
-  // console.log(desiredLength);
-  // console.log(result);
-  // return result;
-// }
-
-
-var library = {
-  1 : "1234567890",
-  2 : ` !"#$%&'()*+,-./:;<=>?@[\]^_{|}~`,
-  3 : "abcdefghijklmnopqrstuvwxyz",
-  4 : "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  }
-
   function generatePassword(){
+
     var desiredLength = prompt("How long would you like your password?");
-    desiredLength = parseInt(desiredLength);
+    desiredLength = parseInt(desiredLength);  
+    var pool = "";
+    var result = "";
+
+  if (desiredLength < 8 || desiredLength > 128){
+   alert("Please choose a number between 8 and 128.");
+   return "";
+  }
+  else{
+    //Stores user choices as booleans in an object
     var choice = {
-      1: confirm("Would you like numbers in your password?"),
+      1 : confirm("Would you like numbers in your password?"),
       2: confirm("Would you like symbols in your password?"),
       3: confirm("Would you like lowercase letters in your password?"),
-      4: confirm("Would you like to have capital letters in your password?")
-          
-  }
-  var pool = function(){
-    for (var i = 1;i < Object.keys(choice).length;i++){
-    if (choice[i]){
-      x += library[i];
+      4: confirm("Would you like to have capital letters in your password?")      
     }
-  }
-    return x;
-  }
-    var result = function(){
-      for (i = 0;i <desiredLength.length,i++;){
-        result += pool[Math.floor(Math.random()*pool.length)];
+    if (!choice[0]&&!choice[1]&&!choice[2]&&!choice[3]){
+            alert("Unable to generate password based on criteria!");
+            return "";
+          }
+    else{
+      //using variable i to reference both choice and library properties
+      for (var i=0 ;i <= Object.keys(choice).length;i++){
+        if (choice[i]){
+        pool += library[i];
         }
+      }
+        
+      for (var index = 0;index <= desiredLength; index++){
+          result += pool[Math.floor(Math.random()*pool.length)];
+          }
+        console.log(pool);
+        console.log(choice);
+      return result;
     }
   }
-
+}
   
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+ 
+
+
